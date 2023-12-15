f0d69d4b9e0cb5bcf4270531b5651f4b4afffc2a
rak-randers <rune.aagaard.keena@randers.dk>
2023-12-15 11:44:09 +0100

    -

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import KeyCloakService from "./security/KeycloakService";
+import KeyCloakService from "./security/keycloakService.js";
 
 const renderApp = () => {
     createApp(App).mount('#app')
